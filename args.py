58dae17172a3b20f986d90e2cbdf48299fb576ec
mxp <maxplayer@free.fr>
2023-04-16 22:31:05 +0200

    adding more tests, not finished work

--- args.py
+++ args.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import List
 '''
 https://codingdojo.org/kata/Args/
 write a parser that takes a schema as input detailing:
@@ -10,11 +11,13 @@ Once schema defined, the programe should send the list to the parser
 
 The parser will check
 '''
-schema_definition = "3"
+schema_definition = "3 l,p,d"
 
 def get_args(schema_definition) -> int:
     return int(schema_definition[0])
 
+def get_flags(schema_definition) -> List[str]:
+    return list(schema_definition.split(' ')[1])
 
 def test_get_schema_definition():
     assert schema_definition is not None
@@ -24,4 +27,4 @@ def test_request_3_args():
     assert 3 == get_args(schema_definition)
 
 def test_get_flags():
-    assert 3 == len(get_flags())
+    assert 3 == len(get_flags(schema_definition))
