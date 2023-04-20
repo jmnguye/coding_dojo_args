3e690659f7fd022b7c78a11811d266d7fab3a2fb
jmnguye <nguyen.jean.michel@gmail.com>
2023-04-20 23:17:05 +0200

    test that the number of args corresond to the number of flags loaded

--- args.py
+++ args.py
@@ -12,10 +12,17 @@ Once schema defined, the programe should send the list to the parser
 The parser will check
 '''
 
+class NumberOfArgsDontMatchNumberOfFlag(Exception):
+    pass
+
 class Parser():
     
     def load(self, schema):
-        self.get_args(schema)
+        number_of_args = self.get_args(schema)
+        number_of_flags = len(self.get_flags(schema))
+        if number_of_args != number_of_flags:
+            raise NumberOfArgsDontMatchNumberOfFlag
+
            
     def get_args(self, schema) -> int:
         return int(schema[0])
@@ -29,12 +36,21 @@ def parser():
     parser = Parser()
     return parser
 
-def test_parser_read_number_args(parser):
+def test_read_number_by(parser):
     schema = "3 l,p,d"
-    parser.load(schema)
     assert 3 == parser.get_args(schema)
 
-def test_parser_miss_number_args(parser):
+def test_miss_number_by(parser):
     schema = "l,p,d"
     with pytest.raises(ValueError):
         parser.get_args(schema)
+
+def test_read_flags_by(parser):
+    schema = "3 l,p,d"
+    assert ['l', 'p', 'd'] == parser.get_flags(schema)
+
+def test_number_of_args_match_number_of_flags(parser):
+    schema = "3 l,p,d,t"
+    with pytest.raises(NumberOfArgsDontMatchNumberOfFlag):
+        parser.load(schema)
+
